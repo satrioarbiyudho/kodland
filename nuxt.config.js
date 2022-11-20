212aa6918f2b5c547039bd42b50b794672bfcdef
Satrio A. Cesiojakty <satrioarbiyudho@gmail.com>
2022-11-20 17:06:43 +0700

    🎉 Initialization

---
+++ nuxt.config.js
@@ -0,0 +1,62 @@
+import colors from 'vuetify/es5/util/colors'
+
+export default {
+  ssr: false,
+  head: {
+    titleTemplate: '%s | Kodland Test',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'This is just a test web page!',
+      },
+      {
+        hid: 'robots',
+        name: 'robots',
+        content: 'index, follow',
+      },
+    ],
+    link: [
+      {
+        hid: 'icon',
+        rel: 'icon',
+        type: 'image/png',
+        href: '/favicon.png',
+      },
+    ],
+  },
+
+  components: true,
+
+  buildModules: ['@nuxtjs/eslint-module', '@nuxtjs/vuetify'],
+
+  modules: [],
+
+  vuetify: {
+    treeShake: true,
+    defaultAssets: {
+      font: {
+        family: 'Montserrat',
+      },
+    },
+    customVariables: ['~/assets/variables.scss'],
+    theme: {
+      light: true,
+      themes: {
+        dark: {
+          primary: colors.blue.darken2,
+          accent: colors.grey.darken3,
+          secondary: colors.amber.darken3,
+          info: colors.teal.lighten1,
+          warning: colors.amber.base,
+          error: colors.deepOrange.accent4,
+          success: colors.green.accent3,
+        },
+      },
+    },
+  },
+
+  build: {},
+}
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-container class="fill-height" fluid> Kodland </v-container>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+  head: {
+    title: 'Welcome!',
+  },
+}
+</script>
